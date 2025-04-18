069c271a466919b19d153478a5476530a2d77307
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-04-18 15:39:47 +0200

    Complete task 3 to reverse the String
    To reverse the string split it first so its characters are stored
    each alone in and array. Then use the reverse method to reverse the
    array. Finally use the join method and specifiy the separator like ""
    to have the result needed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let splitText = text.split("");
+    let reverseArray = splitText.reverse();
+    let joinArray = reverseArray.join("");
+    console.log(joinArray);
+    return joinArray;
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.");
+reverseString("");
+
 
 // Do not edit below this line
 module.exports = reverseString;
