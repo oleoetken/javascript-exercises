1fe95f1bf1c38f68e1442a4b191f006f61e6cdd3
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-04-18 14:26:32 +0200

    Follow instruction on task 1 and 2
    For task 2 remember to better use new variable to refer to inside the
    loop and also to return the result outside of loop for the desired
    outcome in this task or else it will print it out once only

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let newText = "";
+    for (let i = 0; i < num; i++) {
+        newText += text;
+    }
+    return newText;
 };
+
+repeatString("hey", 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("", 10);
+repeatString("goodbye", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
