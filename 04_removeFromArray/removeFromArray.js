0cfdefe07860178be367c2875f5cbdde3edbe934
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-04-21 14:19:46 +0200

    Complete Task 4 with rest parameter
    Use rest to sort everything after comma out and make that rest an array
    Create newArray which is empty first
    Use for each to go over first array and use if statement to check
    wether the array created with the rest includes an element in the first
    array, if it does it doesnt include that element in the newArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // need new array to push the elements to we want to keep
+    const newArray = [];
+
+    array.forEach(element => {
+        // sort out all elements that are equal to args 
+        if(!args.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
