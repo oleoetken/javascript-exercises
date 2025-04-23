c83d986419d459f7ddbe1b5dd72e7f8a2c66583a
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-04-23 13:53:37 +0200

    Solve sumAll with basic for loop first
    First get a generic solution without inputs to check the loop works
    as intended

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+let result = 0;
 
+const sumAll = function() {
+    for (let i = 1; i < 4 +1 ; i++) {
+        // console.log(i);
+        result += i;
+        
+    }
+    // console.log(result);
+    return result;
+    
 };
+console.log(sumAll());
+sumAll();
 
 // Do not edit below this line
 module.exports = sumAll;
